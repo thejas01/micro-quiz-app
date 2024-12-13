83fe6be42b97f5ca9f3555a184d257542735ac97
thejas01 <thejasgs19@gmail.com>
2024-12-13 10:55:51 +0530

    added controller

---
+++ question-service/src/main/java/com/example/question_service/controller/helloController.java
@@ -0,0 +1,16 @@
+package com.example.question_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class helloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+    
+}
