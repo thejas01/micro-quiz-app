d872a5f361c9f66099c8e8e009c4aafcf60fc5e5
thejas01 <thejasgs19@gmail.com>
2024-12-21 14:27:07 +0530

    fixed the  generate endpoint issue

--- question-service/src/main/java/com/example/question_service/dao/QuestionDao.java
+++ question-service/src/main/java/com/example/question_service/dao/QuestionDao.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.question_service.model.Question;
+
 
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
 
-  @Query("SELECT q FROM Question q WHERE q.category = :category ORDER BY FUNCTION('RAND') LIMIT :count")
-List<Question> findRandomQuestionsByCategory(@Param("category") String category, @Param("count") int count);
+    @Query(value = "SELECT q.id FROM question q WHERE q.category = :category ORDER BY RAND() LIMIT 10", nativeQuery = true)
+    List<Integer> findRandomQuestionsByCategory(String category, int numQ);
+
+
+
+    
+
+
 
 }
